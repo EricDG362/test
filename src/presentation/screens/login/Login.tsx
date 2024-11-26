db1ed87131a7714b6946dc43785a5a0f7031569f
eric damian <ericdamian@MacBook-Pro-de-eric.local>
2024-11-26 20:24:36 -0300

    login con crearcuenta

--- src/presentation/screens/login/Login.tsx
+++ src/presentation/screens/login/Login.tsx
@@ -1,19 +1,9 @@
-import React from 'react'
-
-import {Text, TextInput, TouchableWithoutFeedback, KeyboardAvoidingView,
-    ImageBackground, StyleSheet, Platform,Keyboard
-} from 'react-native'
-
-
-
+import React from 'react';
+import { Text, TextInput, TouchableWithoutFeedback, KeyboardAvoidingView, 
+    ImageBackground, StyleSheet, Platform, Keyboard, Pressable, View } from 'react-native';
 
 const Login = () => {
-
-
-        
-
   return (
-
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <KeyboardAvoidingView
         style={estilo.container}
@@ -24,50 +14,88 @@ const Login = () => {
           style={estilo.background}
           resizeMode="cover"
         >
-         
           <TextInput
-            style={estilo.input}
+            style={[estilo.input, estilo.inputPrime]}
             placeholder="INGRESE USUARIO"
             placeholderTextColor={'#000'}
             keyboardType="default"
           />
 
-<TextInput
-            style={[estilo.input,estilo.inputUlti]}
+          <TextInput
+            style={[estilo.input]}
             placeholder="CONTRASEÑA"
             placeholderTextColor={'#000'}
             keyboardType="default"
           />
 
-          <Text>Si no posees una cuenta. CREAR CUENTA</Text>
+          <Pressable
+            style={estilo.boton}
+            onPress={() => {
+              console.log('presionaste el boton');
+            }}
+          >
+            <Text style={estilo.botontext}>INGRESAR</Text>
+          </Pressable>
+
+          {/* Contenedor que alinea el texto y el botón */}
+          <View style={estilo.padre}>
+            <Text style={estilo.text}>Si no posees una cuenta. </Text>
+            <Pressable>
+              <Text style={[estilo.text, estilo.textcreate]}>CREAR CUENTA</Text>
+            </Pressable>
+          </View>
 
         </ImageBackground>
       </KeyboardAvoidingView>
     </TouchableWithoutFeedback>
-  )
+  );
 }
+
 const estilo = StyleSheet.create({
-    container: {
-      flex: 1, // Mantén el 100% de la pantalla
-      
-    },
-    background: {
-      flex: 1, // Hace que la imagen de fondo ocupe todo el espacio disponible
-      justifyContent: 'flex-end', // Alinea el contenido verticalmente
-      alignItems: 'center', // Alinea el contenido horizontalmente
-    },
-    input: {
-      backgroundColor: '#fff',
-      borderRadius: 15,
-      width: '80%',
-      height: 50,
-      paddingLeft: 10,
-      marginBottom:20
-    },
-    inputUlti:{
-        marginBottom:150
-    }
-  });
+  container: {
+    flex: 1, // Mantén el 100% de la pantalla
+  },
+  background: {
+    flex: 1, // Hace que la imagen de fondo ocupe todo el espacio disponible
+    justifyContent: 'center', // Alinea el contenido verticalmente
+    alignItems: 'center', // Alinea el contenido horizontalmente
+  },
+  input: {
+    backgroundColor: '#fff',
+    borderRadius: 15,
+    width: '80%',
+    height: 40,
+    paddingLeft: 10,
+    marginBottom: 20,
+    textAlign: 'center'
+  },
+  inputPrime: {
+    marginTop: 450, // Puedes ajustar esto según el espacio necesario
+  },
+  text: {
+    color: '#ffff',
+    marginTop: 40, // Asegúrate de que este margen no se apile innecesariamente
+  },
+  boton: {
+    backgroundColor: '#ffff',
+    width: '50%',
+    borderRadius: 10,
+    paddingVertical: 15,
+  },
+  botontext: {
+    textAlign: 'center',
+  },
+  // Alineación horizontal y centrado para el texto y el botón de "Crear cuenta"
+  padre: {
+    display: 'flex',
+    flexDirection: 'row', // Esto pone el texto y el botón en una fila
+    alignItems: 'center', // Centra verticalmente
+    justifyContent: 'center', // Centra horizontalmente
+  },
+  textcreate: {
+    color: 'cyan',
+    fontWeight: '700',
+  }
+});
 
-
-export default Login
+export default Login;
